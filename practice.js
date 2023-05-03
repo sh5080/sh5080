e5c3f585c47537973f42f62ead8497a5046ad854
김승환 <prpn97@gmail.com>
2023-05-03 20:57:27 +0900

    큰 수의 법칙

--- practice.js
+++ practice.js
@@ -1,16 +1,25 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split('\n')
-let num = [-3,33,13,1]
 
-let numI = 0
+let M = input[0].split(' ').map(Number)[1] //8
+let N = input[0].split(' ').map(Number)[2] //3
+let arr = input[1].split(' ').map(Number)
+let count = 0;
+let answer = 0;
+arr.sort((a,b)=>a-b)
+let first = arr[arr.length-1]
+let second = arr[arr.length-2]
 
-for(let i = 0; i < num.length; i++){
-    numI+=num[i]
-    if(numI<0){
-    numI-=num[i]
+while(M>0){
+    for( let i = 1; i <= N; i++){
+        answer+=first
+        M-=1
+        if (M === 0) 
+        break
+    }
+    answer+=second
+    M-=1
 }
 
-}
-
-console.log(numI)
+console.log(answer)
 
