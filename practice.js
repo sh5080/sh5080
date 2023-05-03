e4a7d1222494918a7d6f32efa9e0e460097efd4d
김승환 <prpn97@gmail.com>
2023-05-03 22:15:14 +0900

    1이 될 때까지

--- practice.js
+++ practice.js
@@ -1,25 +1,19 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split('\n')
 
-let M = input[0].split(' ').map(Number)[1] //8
-let N = input[0].split(' ').map(Number)[2] //3
-let arr = input[1].split(' ').map(Number)
-let count = 0;
-let answer = 0;
-arr.sort((a,b)=>a-b)
-let first = arr[arr.length-1]
-let second = arr[arr.length-2]
+input = [25,3]
+let N = input[0]
+let K = input[1]
+let count = 0; 
 
-while(M>0){
-    for( let i = 1; i <= N; i++){
-        answer+=first
-        M-=1
-        if (M === 0) 
-        break
-    }
-    answer+=second
-    M-=1
+while(N>1){
+    if(N%K===0){
+        N /= K
+    }else N-=1
+    count+=1
+    
 }
 
-console.log(answer)
+
+console.log(count)
 
