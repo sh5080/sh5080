0c4b10d58b752a5bd5473014f33293b604141796
김승환 <prpn97@gmail.com>
2023-05-04 19:29:04 +0900

    완전탐색 유형

--- practice.js
+++ practice.js
@@ -2,12 +2,17 @@ const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split('\n')
 
 let N = 5
+let count = 0
 let t = ''
-for(let i = 0; i < 60; i++){
+for(let i = 0; i <= N; i++){
     for(let j = 0; j<60; j++){
-        t = i+j
+        for(let k = 0; k < 60; k++){
+            if((String(i)+String(j)+String(k)).includes('3')){
+                count++
+            }
+        }
     }
 }
 
-console.log(t)
+console.log(count)
 
