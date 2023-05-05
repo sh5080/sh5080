7284fa4f0cf5cd7fdc96019112c0a6ab24fe3a91
김승환 <prpn97@gmail.com>
2023-05-05 15:40:01 +0900

    DP 문제풀이

--- practice.js
+++ practice.js
@@ -1,11 +1,27 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split('\n')
 
-let N = '02984'
-let answer = 1
-for(let i = 0; i < N.length; i ++){
-    if(N[i]<2){
-        answer+=Number(N[i])
-    }else answer*=N[i]
+let str = 'K O R E A'
+let count = 450
+
+let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ '
+let num = []
+let result = []
+let answer = ''
+
+for(let i = 65; i <= 90; i++){
+   num.push(i)
 }
+num.push(32)
+
+for(let i = 0; i < str.length; i++){
+    let temp = abc.indexOf(str[i])
+    count -= num[temp]
+    result.push(num[temp])
+    
+    if(count<=0)
+    break
+    answer+=str[i]
+}
+console.log(result)
 console.log(answer)
