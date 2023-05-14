22d35f179266898f9923c183d1ae144f359e4471
김승환 <prpn97@gmail.com>
2023-05-14 22:15:49 +0900

    코테

--- practice.js
+++ practice.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split('\n')
-
-
+let num = input[1].split(' ').map(Number).sort((a,b)=>a-b)
+let sum = 0
+let answer = num.map(a=>sum+=a).reduce((a,b)=>a+b)
+console.log(answer)
 
