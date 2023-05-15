152aad69ec43f122c097a11efed4538dee3db756
김승환 <prpn97@gmail.com>
2023-05-15 20:08:56 +0900

    1110번

--- practice.js
+++ practice.js
@@ -1,7 +1,22 @@
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim().split('\n')
-let num = input[1].split(' ').map(Number).sort((a,b)=>a-b)
-let sum = 0
-let answer = num.map(a=>sum+=a).reduce((a,b)=>a+b)
+let input = fs.readFileSync("./input.txt").toString()
+let num = Number(input)
+let temp = num
+let answer = 0
+let firstNum = 0
+
+    while(true){
+        let splitTemp = temp.toString().split('').map(Number) 
+        if(splitTemp.length<2){
+            splitTemp.unshift(0)
+        }
+        firstNum = (splitTemp[0]+splitTemp[1])%10
+        temp = splitTemp[1]*10+firstNum
+        answer++
+            if(temp===num)
+                break
+    }
+
 console.log(answer)
+
 
