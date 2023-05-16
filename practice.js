048ded8c69cb163c0a9bd874f763892ba776fdbc
김승환 <prpn97@gmail.com>
2023-05-16 23:15:18 +0900

    10810 역추적 알고리즘 풀이

--- practice.js
+++ practice.js
@@ -3,14 +3,13 @@ let input = fs.readFileSync("./input.txt").toString().split('\n')
 let len = input[0].split(' ').map(Number)
 let arr = new Array(len[0]).fill(0)
 
-for (let i = 1; i < input.length; i++) {
-    let num = input[i].split(' ').map(Number)
-    for (let j = num[0] - 1; j < num[1]; j++) {
-      arr[j] = num[2]
+for (let i = input.length - 1; i >= 1; i--) {
+  let num = input[i].split(' ').map(Number)
+  for (let j = num[1] - 1; j >= num[0] - 1; j--) {
+    if(arr[j]===0){
+        arr[j] = num[2]
     }
   }
+}
 let answer = arr.join(' ')
-
 console.log(answer)
-
-
