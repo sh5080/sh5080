cfe9591a00a0d2752025223a868b7db38999f46b
김승환 <prpn97@gmail.com>
2023-05-17 23:04:25 +0900

    10813 문제풀이

--- practice.js
+++ practice.js
@@ -1,9 +1,18 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().split('\n')
 let len = input[0].split(' ').map(Number)
-let arr = new Array(len[0]).fill(0)
+let arr = []
+for(let i = 1; i <= len[0]; i++){
+arr.push(i)
+}
 
-
-
+for(let i = 1; i < input.length; i++){
+    let splitNum = input[i].split(' ').map(Number)
+    let a = splitNum[0]
+    let b = splitNum[1]
+    let temp = arr[a-1];
+        arr[a-1] = arr[b-1];
+        arr[b-1] = temp;
+}
 let answer = arr.join(' ')
 console.log(answer)
