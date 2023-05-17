d97143f89d41c1d4dfa4584881b5f950ef4867ec
김승환 <prpn97@gmail.com>
2023-05-17 23:45:56 +0900

    10811 문제풀이

--- practice.js
+++ practice.js
@@ -5,14 +5,12 @@ let arr = []
 for(let i = 1; i <= len[0]; i++){
 arr.push(i)
 }
-
 for(let i = 1; i < input.length; i++){
     let splitNum = input[i].split(' ').map(Number)
-    let a = splitNum[0]
-    let b = splitNum[1]
-    let temp = arr[a-1];
-        arr[a-1] = arr[b-1];
-        arr[b-1] = temp;
+    let start = splitNum[0]
+    let end = splitNum[1]
+    let reversed = arr.slice(start-1,end).reverse()
+  arr.splice(start-1,reversed.length,...reversed)
 }
 let answer = arr.join(' ')
 console.log(answer)
