9615077edd0e7ac8e282b3af864189c70d07f699
김승환 <prpn97@gmail.com>
2023-05-18 19:01:22 +0900

    27866

--- practice.js
+++ practice.js
@@ -1,12 +1,4 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().split('\n')
-let score = input[1].split(' ').map(Number)
-let sorted = score.sort((a,b)=>b-a)
-let result = 0;
 
-for(let i = 0; i < sorted.length; i++){
-  result += sorted[i]/sorted[0]*100
-}
-let answer = result/input[0]
-console.log(answer)
-
+console.log(input[0][input[1]-1])
