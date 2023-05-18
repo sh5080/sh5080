00d03e082651d4edf4a82af16d228950d8955052
김승환 <prpn97@gmail.com>
2023-05-18 19:46:36 +0900

    11720

--- practice.js
+++ practice.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim()
-
-console.log(input.charCodeAt())
+let input = fs.readFileSync("./input.txt").toString().trim().split('\n')
+let num = input[1].split('').map(Number).reduce((a,b)=>a+b)
+console.log(num)
 
