f4f6f01ec95ca98adfd771cf1cd232188519dd87
김승환 <prpn97@gmail.com>
2023-05-18 19:31:06 +0900

    11654

--- practice.js
+++ practice.js
@@ -1,7 +1,5 @@
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim().split('\n')
+let input = fs.readFileSync("./input.txt").toString().trim()
 
-for(let i = 1; i < input.length; i++){
-console.log(input[i][0]+input[i][input[i].length-1])
-}
+console.log(input.charCodeAt())
 
