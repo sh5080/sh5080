b091f074ded3d616113c113844559e15c9f50c30
김승환 <prpn97@gmail.com>
2023-05-18 18:56:46 +0900

    1546 문제풀이

--- practice.js
+++ practice.js
@@ -1,16 +1,12 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().split('\n')
-let len = input[0].split(' ').map(Number)
-let arr = []
-for(let i = 1; i <= len[0]; i++){
-arr.push(i)
+let score = input[1].split(' ').map(Number)
+let sorted = score.sort((a,b)=>b-a)
+let result = 0;
+
+for(let i = 0; i < sorted.length; i++){
+  result += sorted[i]/sorted[0]*100
 }
-for(let i = 1; i < input.length; i++){
-    let splitNum = input[i].split(' ').map(Number)
-    let start = splitNum[0]
-    let end = splitNum[1]
-    let reversed = arr.slice(start-1,end).reverse()
-  arr.splice(start-1,reversed.length,...reversed)
-}
-let answer = arr.join(' ')
+let answer = result/input[0]
 console.log(answer)
+
