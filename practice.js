50610e01b5ce55c5b3a94defb41ee37dd281d26b
김승환 <prpn97@gmail.com>
2023-05-18 19:59:17 +0900

    10809

--- practice.js
+++ practice.js
@@ -1,5 +1,12 @@
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim().split('\n')
-let num = input[1].split('').map(Number).reduce((a,b)=>a+b)
-console.log(num)
+let input = fs.readFileSync("./input.txt").toString().trim()
+let abc = 'abcdefghijklmnopqrstuvwxyz'
+let result = ''
+for(let i = 0; i < abc.length; i++){
+  if(input.includes(abc[i])){
+    result+=input.indexOf(abc[i])+' '
+  }else result+=(-1)+' '
+}
+
+console.log(result.trim())
 
