b2c4452f120ad7c667307736681865450d3962bc
김승환 <prpn97@gmail.com>
2023-05-19 23:28:19 +0900

    프로그래머스 가장 가까운 같은 글자

--- practice.js
+++ practice.js
@@ -1,10 +1,25 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim()
-let result = input.split(' ')
-let answer = 0
-for(let i = 0; i < result.length; i++){
-  if(result[i]!==''){
-    answer++
+
+let s = 'banana'
+let answer = []
+let temp = []
+for(let i = 0; i < s.length; i++){
+  if(!temp.includes(s[i])){
+      answer.push(-1)
+      temp.push(s[i])
+      //console.log(temp)
+  }else if(temp.includes(s[i])){
+    temp.push(s[i])
+    answer.push(temp.indexOf(s[i])+1)
+    //temp[temp.indexOf(s[i])]=0
+    temp.shift()
+    console.log(temp)
+    console.log(answer)
   }
 }
-console.log(answer)
+
+//console.log(s.includes('b'))
+
+
+// [-1,-1,-1,2,2,2]
