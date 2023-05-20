2b5f1a42850c9d32b41627276d9fc2ad17716907
김승환 <prpn97@gmail.com>
2023-05-21 00:13:40 +0900

    프로그래머스 2016년

--- practice.js
+++ practice.js
@@ -1,24 +1,20 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim()
 
-let name = ["may", "kein", "kain", "radi"];
-let yearning = [5, 10, 1, 3];
-let photo = [
-  ["may", "kein", "kain", "radi"],
-  ["may", "kein", "brin", "deny"],
-  ["kon", "kain", "may", "coni"]
-];
-let answer = [];
-
-for (let i = 0; i < photo.length; i++) {
-  let sum = 0;
-  photo[i].forEach((person) => {
-    sum += yearning[name.indexOf(person)]||0;
-  });
-  answer.push(sum);
+let a = 1
+let b = 1
+let answer = '';
+let day = ['THU','FRI','SAT','SUN','MON','TUE','WED']
+let month = [0,0,31,29,31,30,31,30,31,31,30,31,30]
+let date = b
+for(let i = 0; i <= a; i++){
+    date += month[i]
 }
 
-console.log(answer);
+let result = date%7
+answer=day[result]
+
+console.log(answer)
 
 
 
