7ad696664201a181ef4cbcc265d506ac8a797b7c
김승환 <prpn97@gmail.com>
2023-05-20 21:39:21 +0900

    프로그래머스 가장가까운 같은 글자

--- practice.js
+++ practice.js
@@ -1,25 +1,25 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim()
 
-let s = 'banana'
-let answer = []
+let answer = [];
 let temp = []
-for(let i = 0; i < s.length; i++){
-  if(!temp.includes(s[i])){
-      answer.push(-1)
-      temp.push(s[i])
-      //console.log(temp)
-  }else if(temp.includes(s[i])){
+
+for (let i = 0; i < s.length; i++) {
+  
+  if (!temp.includes(s[i])) {
+    answer.push(-1);
     temp.push(s[i])
-    answer.push(temp.indexOf(s[i])+1)
-    //temp[temp.indexOf(s[i])]=0
-    temp.shift()
-    console.log(temp)
-    console.log(answer)
+    continue
   }
+
+  if(temp.includes(s[i])) { 
+    answer.push(temp.length-temp.lastIndexOf(s[i]))
+    temp.push(s[i])
+  }
+
 }
-
-//console.log(s.includes('b'))
-
+console.log(answer); 
 
 // [-1,-1,-1,2,2,2]
+// [-1,-1,1,-1,-1,-1]
+
