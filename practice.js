40bc6626f9abe7938c0b0f30e9a51dff13e88004
김승환 <prpn97@gmail.com>
2023-05-20 22:49:51 +0900

    프로그래머스 추억 점수

--- practice.js
+++ practice.js
@@ -1,25 +1,26 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim()
 
+let name = ["may", "kein", "kain", "radi"];
+let yearning = [5, 10, 1, 3];
+let photo = [
+  ["may", "kein", "kain", "radi"],
+  ["may", "kein", "brin", "deny"],
+  ["kon", "kain", "may", "coni"]
+];
 let answer = [];
-let temp = []
 
-for (let i = 0; i < s.length; i++) {
-  
-  if (!temp.includes(s[i])) {
-    answer.push(-1);
-    temp.push(s[i])
-    continue
-  }
-
-  if(temp.includes(s[i])) { 
-    answer.push(temp.length-temp.lastIndexOf(s[i]))
-    temp.push(s[i])
-  }
-
+for (let i = 0; i < photo.length; i++) {
+  let sum = 0;
+  photo[i].forEach((person) => {
+    sum += yearning[name.indexOf(person)]||0;
+  });
+  answer.push(sum);
 }
-console.log(answer); 
 
-// [-1,-1,-1,2,2,2]
-// [-1,-1,1,-1,-1,-1]
+console.log(answer);
+
+
+
+
 
