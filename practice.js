f42a61a493f5e01c98c7fc33a8c3b44e714fc41f
김승환 <prpn97@gmail.com>
2023-05-21 19:37:04 +0900

    2908

--- practice.js
+++ practice.js
@@ -1,19 +1,14 @@
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim()
-
-let a = ["problemsolving", "practiceguitar", "swim", "studygraph"]
-let b = [true, false, true, false]
-
-let answer = [];
-for(let i = 0; i < a.length; i++){
-  if(b[i]===false){
-    answer.push(a[i])
-  }  
-}
-
-console.log(answer)
+let input = fs.readFileSync("./input.txt").toString().trim().split(' ')
+input = input.map((a)=>{
+    let reversed = a.split('').reverse().join('')
+    return reversed
+               })
+    input.map(Number)
+console.log(input[0]>input[1]?input[0]:input[1])
 
 
+//437
 
 
 
