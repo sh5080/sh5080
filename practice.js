ec30a4e842594c6c79fc79e6aa350f04b70c7c7c
김승환 <prpn97@gmail.com>
2023-05-21 20:54:33 +0900

    5622

--- practice.js
+++ practice.js
@@ -1,15 +1,20 @@
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim().split(' ')
-input = input.map((a)=>{
-    let reversed = a.split('').reverse().join('')
-    return reversed
-               })
-    input.map(Number)
-console.log(input[0]>input[1]?input[0]:input[1])
-
-
-//437
-
+let input = fs.readFileSync("./input.txt").toString().trim().split('')
+let result = []
+let dial = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
+let abc = dial.map((a)=>{
+  let splited = a.split('')
+  return splited
+})
+for(let i = 0; i < input.length; i++){
+  for(let j = 0; j < abc.length; j++){
+    if(abc[j].includes(input[i])){
+      result.push(abc.indexOf(abc[j])+2)
+    }
+  }
+}
+let answer = result.reduce((a,b)=>a+b)+input.length
+console.log(answer)
 
 
 
