ddac35572a149e8cc21283bc22a41dd0b3c62b30
김승환 <prpn97@gmail.com>
2023-05-23 23:45:53 +0900

    2738

--- practice.js
+++ practice.js
@@ -1,22 +1,32 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split('\n');
-let mypoint = 0
-let mygrade = ''
-let result = 0
-let gradeToPoint = [4.5,4.0,3.5,3.0,2.5,2.0,1.5,1.0,0.0]
-let grade = ['A+','A0','B+','B0','C+','C0','D+','D0','F']
-let wholePoint = 0
-for(let i = 0; i < input.length; i++){
-  let splited = input[i].split(' ')
-  mypoint = Number(splited[1])
-  mygrade = splited[2]
-if(mygrade!=='P'){
-result+=mypoint*(gradeToPoint[grade.indexOf(mygrade)])
-wholePoint+= mypoint
+
+const [N, M] = input[0].trim().split(' ').map(Number);
+
+// 행렬 A 입력 받기
+let matrix_A = [];
+for (let i = 1; i <= N; i++) {
+  matrix_A.push(input[i].trim().split(' ').map(Number));
 }
+
+// 행렬 B 입력 받기
+let matrix_B = [];
+for (let i = N + 1; i <= 2 * N; i++) {
+  matrix_B.push(input[i].trim().split(' ').map(Number));
 }
-console.log(result/wholePoint)
 
+// 행렬의 합 계산
+let matrix_sum = [];
+for (let i = 0; i < N; i++) {
+  let row_sum = [];
+  for (let j = 0; j < M; j++) {
+    let element_sum = matrix_A[i][j] + matrix_B[i][j];
+    row_sum.push(element_sum);
+  }
+  matrix_sum.push(row_sum);
+}
 
-
-
+// 결과 출력
+for (let i = 0; i < N; i++) {
+  console.log(matrix_sum[i].join(' '));
+}
