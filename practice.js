698359ddabc72dea7bbbee9bb38d47f622b6f07a
김승환 <prpn97@gmail.com>
2023-05-24 20:27:36 +0900

    2566

--- practice.js
+++ practice.js
@@ -1,32 +1,22 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split('\n');
-
-const [N, M] = input[0].trim().split(' ').map(Number);
-
-// 행렬 A 입력 받기
-let matrix_A = [];
-for (let i = 1; i <= N; i++) {
-  matrix_A.push(input[i].trim().split(' ').map(Number));
+let result = []
+let arr = []
+let X = 0
+let Y = 0
+for(let i = 0; i < input.length; i++){
+  let row = input[i].split(' ').map(Number)
+  let max = row.reduce((a,b)=>Math.max(a,b))
+  arr.push(row)
+  result.push(max)
 }
-
-// 행렬 B 입력 받기
-let matrix_B = [];
-for (let i = N + 1; i <= 2 * N; i++) {
-  matrix_B.push(input[i].trim().split(' ').map(Number));
+let max = result.reduce((a,b)=>Math.max(a,b))
+for(let i = 0; i < arr.length; i++){
+  arr[i].indexOf(max) > -1 ? X = i : 0 //arr[i].indexOf(max)+1 : 0
 }
+console.log(max)
 
-// 행렬의 합 계산
-let matrix_sum = [];
-for (let i = 0; i < N; i++) {
-  let row_sum = [];
-  for (let j = 0; j < M; j++) {
-    let element_sum = matrix_A[i][j] + matrix_B[i][j];
-    row_sum.push(element_sum);
-  }
-  matrix_sum.push(row_sum);
-}
+Y = arr[X].indexOf(max)
+console.log(X+1,Y+1)
 
-// 결과 출력
-for (let i = 0; i < N; i++) {
-  console.log(matrix_sum[i].join(' '));
-}
+
