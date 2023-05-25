6657a0767baf27c1aad63bc045d58b6b7fba76e5
김승환 <prpn97@gmail.com>
2023-05-25 23:47:03 +0900

    명예의 전당(1)

--- practice.js
+++ practice.js
@@ -1,26 +1,18 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split('\n');
 
-
-function binary_search(arr,item){
-let low = 0
-let high = arr.length-1
-
-
-while(low<= high){
-  let mid = Math.floor((low + high)/2)
-  let guess = arr[mid]
-  if(guess < item){
-    low = mid + 1
-  }if( guess === item){
-    return mid
-  }if ( guess > item){
-    high = mid - 1
-  }else {
-    low = mid+1
+function solution(k, score) {
+  let answer = [];
+  let temp = []
+  
+  for(let i = 0; i < score.length; i++){
+      temp.push(score[i])
+      temp.sort((a,b)=>b-a)
+      if(temp.length>k)
+          temp.pop()
+      answer.push(temp[temp.length-1])
   }
-}
-}
-console.log(binary_search([1,3,5,7,9],7)) //3
 
+  return answer
+}console.log(solution(3,[10, 100, 20, 150, 1, 100, 200]))
 
