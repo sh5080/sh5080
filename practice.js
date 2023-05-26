bfeec5e4b554bd41dbb83b6462ac2733166e726a
김승환 <prpn97@gmail.com>
2023-05-26 22:16:20 +0900

    update

--- practice.js
+++ practice.js
@@ -1,18 +1,23 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split('\n');
 
-function solution(k, score) {
-  let answer = [];
-  let temp = []
-  
-  for(let i = 0; i < score.length; i++){
-      temp.push(score[i])
-      temp.sort((a,b)=>b-a)
-      if(temp.length>k)
-          temp.pop()
-      answer.push(temp[temp.length-1])
-  }
+function solution(n) { 
+  let arr = [] 
+  // 1은 소수가 아니고, 2부터 소수가 될 수 있으므로, 2부터 구하고자 하는 값까지의 배열을 만든다. 
+  for (let i = 2; i <= n; i++) { 
+      arr[i] = i 
+  } 
+  // 2부터 시작해서 2배수 이상의 숫자를 모두 지우되, 이미 지워진 숫자는 건너 뛴다. 
+  for (let i = 2; i <= n; i++) { 
+      for (let j = i + i; j <= n; j += i) { 
+          if (arr[j] === 0) { 
+              continue 
+          } 
+          arr[j] = 0 
+      } 
+  } 
+  return arr.filter((item) => item !== 0).length 
+}
 
-  return answer
-}console.log(solution(3,[10, 100, 20, 150, 1, 100, 200]))
+console.log(solution(10))
 
