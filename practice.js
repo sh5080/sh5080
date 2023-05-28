4f34b7765cf5b8add65e77eff39cac3d814fc633
김승환 <prpn97@gmail.com>
2023-05-28 18:35:48 +0900

    update

--- practice.js
+++ practice.js
@@ -1,34 +1,24 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split('\n');
 
-let nums = [1,2,7,6,4]
-  let add = []
-  let temp = []
+function solution(cards1, cards2, goal) {
 
-for(let i = 0; i < nums.length; i++){
-    for(let j = i+1; j < nums.length; j++){
-        for(let k = j+1; k < nums.length; k++){
-            add.push(nums[i]+nums[j]+nums[k])
-    }
-    }
-}
-for(let i = 0; i < add.length; i++){
-  if(add[i]%2!==0){
-let isPrime = true;
-   for(let j = 3; j <= Math.sqrt(add[i]);j+=2){
-    if(add[i]%j===0){
-      isPrime = false;
-      break;
-    }
-   }
-   if(isPrime)     
-   temp.push(add[i])
-  }
-}
-console.log(temp)
-console.log(add) 
-// console.log(count)
-console.log(Math.sqrt(6))
-// 9 7 11 15 13 17
-//2 5 
+let num1 = cards1.map((a)=>goal.indexOf(a))
+let num2 = cards2.map((a)=>goal.indexOf(a))
+
+let sorted1 = [...num1].sort((a,b)=>a-b) 
+let sorted2 = [...num2].sort((a,b)=>a-b)
+
+let isEqual1 = num1.every((value, index) => value === sorted1[index]);
+console.log(isEqual1); 
+let isEqual2 = num2.every((value, index) => value === sorted2[index]);
+console.log(isEqual2); 
+
+
+return isEqual1 && isEqual2
+?  "Yes" : "No"
+
+
+
+}console.log(solution(["i", "drink", "water"],["want", "to"],["i", "want", "to", "drink", "water"]))
 
