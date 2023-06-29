321f43f35e0e7f6448d42b6c11c27a5e23981a7d
김승환 <prpn97@gmail.com>
2023-06-29 20:06:55 +0900

    프로그래머스 등차수열의 특정한 항만 더하기

--- practice.js
+++ practice.js
@@ -1,22 +1,23 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(numLog) {
-    var answer = '';
- for(let i = 1; i < numLog.length; i++){
-     if(numLog[i]===numLog[i-1]+1){
-         answer+='w'
-     }else if(numLog[i]===numLog[i-1]-1){
-         answer+='s'
-     }else if(numLog[i]===numLog[i-1]+10){
-         answer+='d'
-     }else if (numLog[i]===numLog[i-1]-10)
-         answer+='a'
- }
+function solution(a, d, included) {
+    let answer = 0;
+    let inc = []
+
+    for(let i = a; i <=a+(d*(included.length-1)); i+=d){
+        inc.push(i)
+    }
+    for(let i = 0; i < inc.length; i++){
+        if(included[i]===true){
+            answer+=inc[i]
+            console.log(answer)
+        }
+    }
     return answer;
 }
 
-console.log('1:',solution([0,1,0,10,0,1,0,10,0,-1,-2,-1]));
+console.log('1:',solution(3,4,[true,false,false,true,true]));
 // console.log('2:',solution("dxccxbbbxaaaa"));
 // console.log('3:',solution("axbbxbxxxbbbbxxxxxcccc"));
 // console.log('4:',solution("axbbxbxxxbbbbxxxxxccccxbb"));
