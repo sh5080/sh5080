23f81d418382c6f32f31088a4fa0b0625dcc5a5d
김승환 <prpn97@gmail.com>
2023-06-29 17:30:02 +0900

    프로그래머스 수 조작하기2

--- practice.js
+++ practice.js
@@ -1,21 +1,22 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(myString) {
-  let abc = "0abcdefghijklmnopqrstuvwxyz";
-  let answer = myString.split("x");
-  let result = [];
-  answer.push("x");
-  for (let i = 0; i < answer.length - 1; i++) {
-    if (answer[i] !== "") {
-      result.push(answer[i]);
-    }
-  }
-
-  return result.sort();
+function solution(numLog) {
+    var answer = '';
+ for(let i = 1; i < numLog.length; i++){
+     if(numLog[i]===numLog[i-1]+1){
+         answer+='w'
+     }else if(numLog[i]===numLog[i-1]-1){
+         answer+='s'
+     }else if(numLog[i]===numLog[i-1]+10){
+         answer+='d'
+     }else if (numLog[i]===numLog[i-1]-10)
+         answer+='a'
+ }
+    return answer;
 }
 
-console.log('1:',solution("axbxcxdx"));
-console.log('2:',solution("dxccxbbbxaaaa"));
-console.log('3:',solution("axbbxbxxxbbbbxxxxxcccc"));
-console.log('4:',solution("axbbxbxxxbbbbxxxxxccccxbb"));
+console.log('1:',solution([0,1,0,10,0,1,0,10,0,-1,-2,-1]));
+// console.log('2:',solution("dxccxbbbxaaaa"));
+// console.log('3:',solution("axbbxbxxxbbbbxxxxxcccc"));
+// console.log('4:',solution("axbbxbxxxbbbbxxxxxccccxbb"));
