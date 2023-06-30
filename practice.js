4be98abb640223e31772bace06a36473804a4cd1
김승환 <prpn97@gmail.com>
2023-06-30 20:25:20 +0900

    프로그래머스 배열 만들기 5

--- practice.js
+++ practice.js
@@ -1,23 +1,14 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(a, d, included) {
-    let answer = 0;
-    let inc = []
-
-    for(let i = a; i <=a+(d*(included.length-1)); i+=d){
-        inc.push(i)
-    }
-    for(let i = 0; i < inc.length; i++){
-        if(included[i]===true){
-            answer+=inc[i]
-            console.log(answer)
-        }
-    }
+function solution(intStrs, k, s, l) {
+    let result = intStrs.map((a)=>a.slice(s,s+l))
+   let answer = result.filter((a)=>a>k).map(Number)
     return answer;
 }
 
-console.log('1:',solution(3,4,[true,false,false,true,true]));
+console.log('1:',solution(['0123456789','9876543210','9999999999999'],50000,5,5));
+
 // console.log('2:',solution("dxccxbbbxaaaa"));
 // console.log('3:',solution("axbbxbxxxbbbbxxxxxcccc"));
 // console.log('4:',solution("axbbxbxxxbbbbxxxxxccccxbb"));
