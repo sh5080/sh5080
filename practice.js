e1771d3aa1bf5c4a97c1e2846a9cf05e4d6a813e
김승환 <prpn97@gmail.com>
2023-06-30 20:39:08 +0900

    프로그래머스 날짜 비교하기

--- practice.js
+++ practice.js
@@ -1,13 +1,16 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(intStrs, k, s, l) {
-    let result = intStrs.map((a)=>a.slice(s,s+l))
-   let answer = result.filter((a)=>a>k).map(Number)
-    return answer;
+function solution(date1, date2) {
+    for (let i = 0; i < 3; i++) {
+        if (date1[i] < date2[i]) return 1;
+        else if (date1[i] > date2[i]) return 0;
+    }
+    return 0; 
 }
 
-console.log('1:',solution(['0123456789','9876543210','9999999999999'],50000,5,5));
+
+console.log('1:',solution([2022, 1, 1], [2023, 1, 1]));
 
 // console.log('2:',solution("dxccxbbbxaaaa"));
 // console.log('3:',solution("axbbxbxxxbbbbxxxxxcccc"));
