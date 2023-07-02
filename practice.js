499d693b7bd1a54daec066ec10556d93fd53cd94
김승환 <prpn97@gmail.com>
2023-07-02 19:29:59 +0900

    프로그래머스 이차원 배열 대각선 순회하기

--- practice.js
+++ practice.js
@@ -1,17 +1,19 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
-function solution(my_string, indices) {
-    let splited = my_string.split('')
-            for(let j = 0; j < indices.length; j++){
-                (splited[indices[j]]) = '*'
-            }
-    splited = splited.join('')
-    let answer = splited.replaceAll('*','')
-        return answer;
+
+function solution(board, k) {
+    let answer = 0
+for(let i = 0; i < board.length; i++){
+    for(let j = 0; j < board[i].length; j++){
+        if(i + j <= k){
+            answer+=(board[i][j])
+        }
     }
+}
+    return answer;
+}
 
-
-console.log('1:',solution("apporoograpemmemprs", [1, 16, 6, 15, 0, 10, 11, 3]));
+console.log('1:',solution([[0, 1, 2],[1, 2, 3],[2, 3, 4],[3, 4, 5]], 2));
 
 // console.log('2:',solution("dxccxbbbxaaaa"));
 // console.log('3:',solution("axbbxbxxxbbbbxxxxxcccc"));
