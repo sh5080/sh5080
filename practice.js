bd062a9de0fc10296494fe2ca68698cd87daebd0
김승환 <prpn97@gmail.com>
2023-07-02 18:38:48 +0900

    프로그래머스 수열과 구간 쿼리 1

--- practice.js
+++ practice.js
@@ -1,16 +1,20 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(date1, date2) {
-    for (let i = 0; i < 3; i++) {
-        if (date1[i] < date2[i]) return 1;
-        else if (date1[i] > date2[i]) return 0;
+function solution(arr, queries) {
+    for(let i = 0; i < queries.length; i++){
+        const [s,e] = queries[i]
+        for(let j = s; j <= e; j++){
+            arr[j] += 1
+        }
     }
-    return 0; 
+ 
+    
+    return arr
 }
 
 
-console.log('1:',solution([2022, 1, 1], [2023, 1, 1]));
+console.log('1:',solution([0,1,2,3,4], [[0,1],[1,2],[2,3]]));
 
 // console.log('2:',solution("dxccxbbbxaaaa"));
 // console.log('3:',solution("axbbxbxxxbbbbxxxxxcccc"));
