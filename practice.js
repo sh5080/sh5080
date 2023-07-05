0e98ff857010917c1ff7620c82605016817dce06
김승환 <prpn97@gmail.com>
2023-07-05 18:44:59 +0900

    프로그래머스 문자열이 몇 번 등장하는지 세기

--- practice.js
+++ practice.js
@@ -2,18 +2,19 @@ const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
 function solution(myString, pat) {
-
-    let length_pat = myString.lastIndexOf(pat)
-    let answer = myString.slice(0,length_pat+pat.length)
-    return answer;
+  let answer = 0;
+  let result = 0;
+  for (let i = 0; i < myString.length; i++) {
+    answer = myString.slice(i, i + pat.length);
+    if (answer === pat) {
+      result++;
+    }
+  }
+  return result;
 }
 
-console.log("answer1:", solution(
-    "AbCdEFG","dE"
-    ));
-    console.log("answer2:", solution(
-        "AAAAaaaa","a"
-        ));
+console.log("answer1:", solution("banana", "ana"));
+console.log("answer2:", solution("aaaa", "aa"));
 
 // console.log('2:',solution("dxccxbbbxaaaa"));
 // console.log('3:',solution("axbbxbxxxbbbbxxxxxcccc"));
