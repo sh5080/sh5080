c0493a41e902028c1569e95bc703c2d9d665d50b
김승환 <prpn97@gmail.com>
2023-07-06 20:14:22 +0900

    프로그래머스

--- practice.js
+++ practice.js
@@ -1,21 +1,10 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(myString, pat) {
-  let answer = 0;
-  let result = 0;
-  for (let i = 0; i < myString.length; i++) {
-    answer = myString.slice(i, i + pat.length);
-    if (answer === pat) {
-      result++;
-    }
-  }
+function solution(x1, x2, x3, x4) {
+  const result = (x1 || x2) && (x3 || x4);
   return result;
 }
 
-console.log("answer1:", solution("banana", "ana"));
-console.log("answer2:", solution("aaaa", "aa"));
-
-// console.log('2:',solution("dxccxbbbxaaaa"));
-// console.log('3:',solution("axbbxbxxxbbbbxxxxxcccc"));
-// console.log('4:',solution("axbbxbxxxbbbbxxxxxccccxbb"));
+console.log("answer1:", solution("false", "true", "true", "true"));
+console.log("answer2:", solution("true", "false", "false", "false"));
