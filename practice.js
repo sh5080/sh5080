63f201067ebab5b06fdaf900c76c08069c1fe9d6
김승환 <prpn97@gmail.com>
2023-08-04 21:50:28 +0900

    프로그래머스 세 개의 구분자

--- practice.js
+++ practice.js
@@ -1,20 +1,25 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-const readline = require("readline");
+function solution(myStr) {
+  var answer = [];
+  let a = myStr.replaceAll("a", "*");
+  let b = a.replaceAll("b", "*");
+  let c = b.replaceAll("c", "*");
+  let result = c.split("*");
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] !== "") {
+      answer.push(result[i]);
+    }
+  }
+  if (answer[0] === undefined) {
+    answer.push("EMPTY");
+  }
+  return answer;
+}
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.on("line", function (x) {
-  console.log(x);
-
-  rl.close();
-}).on("close", function () {
-  process.exit();
-});
-
-// console.log("answer1:", solution([1, 2, 3, 4, 5, 6]));
+console.log(
+  "answer1:",
+  solution("baconlettucetomato", ["onlettu", "etom", "to"])
+);
 // console.log("answer2:", solution([58, 172, 746, 89]));
