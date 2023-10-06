b0a0cfe5724c30f38efd5a0102accc7d4a54ea70
김승환 <prpn97@gmail.com>
2023-10-06 15:42:17 +0900

    feat: 프로그래머스 수열과 구간 쿼리 4

--- practice.js
+++ practice.js
@@ -1,25 +1,18 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(myStr) {
-  var answer = [];
-  let a = myStr.replaceAll("a", "*");
-  let b = a.replaceAll("b", "*");
-  let c = b.replaceAll("c", "*");
-  let result = c.split("*");
-  for (let i = 0; i < result.length; i++) {
-    if (result[i] !== "") {
-      answer.push(result[i]);
+function solution(arr, queries) {
+  for (let i = 0; i < queries.length; i++) {
+    const [start, end, divisor] = queries[i];
+
+    for (let j = start; j <= end; j++) {
+      if (j % divisor === 0) {
+        arr[j] += 1;
+      }
     }
   }
-  if (answer[0] === undefined) {
-    answer.push("EMPTY");
-  }
-  return answer;
+
+  return arr;
 }
 
-console.log(
-  "answer1:",
-  solution("baconlettucetomato", ["onlettu", "etom", "to"])
-);
 // console.log("answer2:", solution([58, 172, 746, 89]));
