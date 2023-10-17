76c0f0209f7af680b41619119d82f33fcfc95da4
김승환 <122625110+sh5080@users.noreply.github.com>
2023-10-17 20:49:03 +0900

    프로그래머스 2단계 최댓값과 최솟값

--- practice.js
+++ practice.js
@@ -1,30 +1,15 @@
 const fs = require("fs");
 let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(arr) {
-    var answer = [];
-    let i = 0
-    let stk = [];
-    while(i < arr.length){
-        if(stk.length === 0){
-            stk.push(arr[i])
-            i++;
-        }else if(stk.length !== 0
-            && stk[stk.length-1] === arr[i]
-                ){
-            stk.pop()
-            i++;
-        }else if(stk.length !== 0 
-            && stk[stk.length-1] !== arr[i]
-        ){
-            stk.push(arr[i])
-            i++;
-        }
-    }
-    if(stk.length === 0){
-        return [-1]
-    }else return stk;
+function solution(s) {
+    var answer = '';
+    let arr = s.split(' ').map(Number).sort((a,b)=>a-b)
+    let first = String(arr[0])
+    let last = String(arr[arr.length-1])
+    
+    return first + ' ' + last;
 }
+
 // console.log(
 //   "answer1:",
 //   solution("baconlettucetomato", ["onlettu", "etom", "to"])
