3bed47f1a41c4318f601cacd84c92d221ae6b754
김승환 <prpn97@gmail.com>
2023-10-29 16:26:46 +0900

    백준 1316 그룹 단어 체커

--- practice.js
+++ practice.js
@@ -1,30 +1,32 @@
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().trim().split("\n");
+const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(arr, queries) {
-    var answer = [];
-    for(let i = 0; i < queries.length; i++) {
-        let [s, e, k] = queries[i];
-        let min = Infinity; 
-        
-        for(let j = s; j <= e; j++) {
-            if(arr[j] > k && arr[j] < min) {
-                min = arr[j];
-            }
-        }
-        
-        if(min === Infinity) {
-            answer.push(-1); 
-        } else {
-            answer.push(min);
-        }
+function isGroupWord(word) {
+  const visited = new Set();
+  for (let i = 0; i < word.length; i++) {
+    if (visited.has(word[i])) {
+      if (word[i] !== word[i - 1]) {
+        return false;
+      }
+    } else {
+      visited.add(word[i]);
     }
-    return answer;
+  }
+  return true;
 }
 
+let count = 0;
+
+for (let i = 1; i < input.length; i++) {
+  if (isGroupWord(input[i])) {
+    count++;
+  }
+}
+
+console.log(count);
 
 // console.log(
 //   "answer1:",
 //   solution("baconlettucetomato", ["onlettu", "etom", "to"])
 // );
-console.log("answer2:", solution([1, 2, 1, 4, 5, 2, 9]));
+// console.log("answer2:", solution([1, 2, 1, 4, 5, 2, 9]));
