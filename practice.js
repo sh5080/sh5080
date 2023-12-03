43a7668a011d241b5f12821cb4b11b1de21fc501
sh5080 <prpn97@gmail.com>
2023-12-03 18:56:10 +0900

    프로그래머스 0단계 정사각형으로 만들기

--- practice.js
+++ practice.js
@@ -1,23 +1,45 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(arr, k) {
-  var answer = [];
+function solution(arr) {
   for (let i = 0; i < arr.length; i++) {
-    if (!answer.includes(arr[i])) {
-      answer.push(arr[i]);
-      if (answer.length === k) {
-        break;
+    if (arr.length === arr[i].length) {
+      return arr;
+    }
+    if (arr.length > arr[i].length) {
+      while (arr[i].length < arr.length) {
+        arr[i].push(0);
+      }
+    }
+    if (arr[i].length > arr.length) {
+      while (arr.length < arr[i].length) {
+        arr.push(new Array(arr[i].length).fill(0));
       }
     }
   }
-  if (answer.length <= k) {
-    while (answer.length < k) {
-      answer.push(-1);
-    }
-  }
-  return answer;
+  return arr;
 }
-console.log("answer1:", solution([0, 1, 1, 2, 2, 3], 3));
-console.log("answer2:", solution([0, 1, 1, 1, 1], 4));
+console.log(
+  "answer1:",
+  solution([
+    [572, 22, 37],
+    [287, 726, 384],
+    [85, 137, 292],
+    [487, 13, 876],
+  ])
+);
+console.log(
+  "answer2:",
+  solution([
+    [57, 192, 534, 2],
+    [9, 345, 192, 999],
+  ])
+);
+console.log(
+  "answer3:",
+  solution([
+    [1, 2],
+    [3, 4],
+  ])
+);
 // console.log("answer2:", solution([1, 2, 1, 4, 5, 2, 9]));
