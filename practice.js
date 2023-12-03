f569c363bbe5eec6a946dd28e9843daefc33b2ae
sh5080 <prpn97@gmail.com>
2023-12-03 18:36:37 +0900

    프로그래머스 0단계 무작위로 k개의 수 뽑기

--- practice.js
+++ practice.js
@@ -1,32 +1,23 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function isGroupWord(word) {
-  const visited = new Set();
-  for (let i = 0; i < word.length; i++) {
-    if (visited.has(word[i])) {
-      if (word[i] !== word[i - 1]) {
-        return false;
+function solution(arr, k) {
+  var answer = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!answer.includes(arr[i])) {
+      answer.push(arr[i]);
+      if (answer.length === k) {
+        break;
       }
-    } else {
-      visited.add(word[i]);
     }
   }
-  return true;
-}
-
-let count = 0;
-
-for (let i = 1; i < input.length; i++) {
-  if (isGroupWord(input[i])) {
-    count++;
+  if (answer.length <= k) {
+    while (answer.length < k) {
+      answer.push(-1);
+    }
   }
+  return answer;
 }
-
-console.log(count);
-
-// console.log(
-//   "answer1:",
-//   solution("baconlettucetomato", ["onlettu", "etom", "to"])
-// );
+console.log("answer1:", solution([0, 1, 1, 2, 2, 3], 3));
+console.log("answer2:", solution([0, 1, 1, 1, 1], 4));
 // console.log("answer2:", solution([1, 2, 1, 4, 5, 2, 9]));
