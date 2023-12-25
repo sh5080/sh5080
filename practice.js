a8c4988ab3d9b7a88ce28be72d0453d514da50bd
sh5080 <prpn97@gmail.com>
2023-12-25 23:49:19 +0900

    프로그래머스 전국 대회 선발 고사

--- practice.js
+++ practice.js
@@ -1,21 +1,33 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(my_string, overwrite_string, s) {
-  let answer = "";
-  let first = my_string.slice(0, s);
-  let last = my_string.slice(s + overwrite_string.length);
-  if (s === 0) {
-    answer = overwrite_string + last;
-  } else if (s + overwrite_string.length < my_string.length) {
-    answer = first + overwrite_string + last;
-  } else answer = first + overwrite_string;
+function solution(rank, attendance) {
+  let answer = 0;
+  const originRank = [...rank];
+  const sortRank = rank.sort((a, b) => a - b);
+  let sortAttendance = [];
+  for (let i = 0; i < rank.length; i++) {
+    sortAttendance.push(attendance[originRank.indexOf(sortRank[i])]);
+  }
+  const trueArr = sortAttendance
+    .map((v, i) => (v === true ? i + 1 : undefined))
+    .filter((index) => index !== undefined);
 
+  const answerRank = trueArr.map((v) => originRank.indexOf(v));
+  answer = answerRank[0] * 10000 + answerRank[1] * 100 + answerRank[2];
   return answer;
 }
 
-console.log("answer1:", solution("He11oWor1d", "lloWorl", 2));
-console.log("answer2:", solution("Program29b8UYP", "merS123", 7));
-console.log("answer3:", solution("aaaaaa", "bbb", 3));
-console.log("answer4:", solution("aaaaaa", "bbb", 2));
-// console.log("answer2:", solution([1, 2, 1, 4, 5, 2, 9]));
+console.log(
+  "answer1:",
+  solution(
+    [3, 7, 2, 5, 4, 6, 1],
+    [false, true, true, true, true, false, false],
+    20403
+  )
+);
+console.log("answer2:", solution([1, 2, 3], [true, true, true], 102));
+console.log(
+  "answer3:",
+  solution([6, 1, 5, 2, 3, 4], [true, false, true, false, false, true], 50200)
+);
