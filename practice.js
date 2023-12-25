e11440120b04ca0ac062bf5afbfdd12395681313
sh5080 <prpn97@gmail.com>
2023-12-25 19:44:57 +0900

    프로그래머스 그림확대

--- practice.js
+++ practice.js
@@ -1,45 +1,35 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr.length === arr[i].length) {
-      return arr;
+function solution(picture, k) {
+  var answer = [];
+  let temp = "";
+  picture.map((a) => {
+    for (let i = 0; i < a.length; i++) {
+      temp += a[i].repeat(k);
     }
-    if (arr.length > arr[i].length) {
-      while (arr[i].length < arr.length) {
-        arr[i].push(0);
-      }
+    for (let j = 0; j < k; j++) {
+      answer.push(temp);
     }
-    if (arr[i].length > arr.length) {
-      while (arr.length < arr[i].length) {
-        arr.push(new Array(arr[i].length).fill(0));
-      }
-    }
-  }
-  return arr;
+
+    temp = "";
+  });
+  return answer;
 }
 console.log(
   "answer1:",
-  solution([
-    [572, 22, 37],
-    [287, 726, 384],
-    [85, 137, 292],
-    [487, 13, 876],
-  ])
+  solution(
+    [
+      ".xx...xx.",
+      "x..x.x..x",
+      "x...x...x",
+      ".x.....x.",
+      "..x...x..",
+      "...x.x...",
+      "....x....",
+    ],
+    2
+  )
 );
-console.log(
-  "answer2:",
-  solution([
-    [57, 192, 534, 2],
-    [9, 345, 192, 999],
-  ])
-);
-console.log(
-  "answer3:",
-  solution([
-    [1, 2],
-    [3, 4],
-  ])
-);
+
 // console.log("answer2:", solution([1, 2, 1, 4, 5, 2, 9]));
