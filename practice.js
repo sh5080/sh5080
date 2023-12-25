056a1b063412100286faec982930d7ef0ab70b33
sh5080 <prpn97@gmail.com>
2023-12-25 21:00:06 +0900

    프로그래머스 문자열 겹쳐쓰기

--- practice.js
+++ practice.js
@@ -1,35 +1,21 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(picture, k) {
-  var answer = [];
-  let temp = "";
-  picture.map((a) => {
-    for (let i = 0; i < a.length; i++) {
-      temp += a[i].repeat(k);
-    }
-    for (let j = 0; j < k; j++) {
-      answer.push(temp);
-    }
+function solution(my_string, overwrite_string, s) {
+  let answer = "";
+  let first = my_string.slice(0, s);
+  let last = my_string.slice(s + overwrite_string.length);
+  if (s === 0) {
+    answer = overwrite_string + last;
+  } else if (s + overwrite_string.length < my_string.length) {
+    answer = first + overwrite_string + last;
+  } else answer = first + overwrite_string;
 
-    temp = "";
-  });
   return answer;
 }
-console.log(
-  "answer1:",
-  solution(
-    [
-      ".xx...xx.",
-      "x..x.x..x",
-      "x...x...x",
-      ".x.....x.",
-      "..x...x..",
-      "...x.x...",
-      "....x....",
-    ],
-    2
-  )
-);
 
+console.log("answer1:", solution("He11oWor1d", "lloWorl", 2));
+console.log("answer2:", solution("Program29b8UYP", "merS123", 7));
+console.log("answer3:", solution("aaaaaa", "bbb", 3));
+console.log("answer4:", solution("aaaaaa", "bbb", 2));
 // console.log("answer2:", solution([1, 2, 1, 4, 5, 2, 9]));
