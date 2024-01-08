05462c57ef77cc50380f9865c6c6005315d4cbb9
sh5080 <prpn97@gmail.com>
2024-01-09 00:08:30 +0900

    프로그래머스 최솟값 만들기

--- practice.js
+++ practice.js
@@ -1,20 +1,17 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(code) {
-  let mode = 0;
-  let ret = "";
-  const codeArray = code.split("");
+function solution(a, b) {
+  let answer = 0;
+  a.sort((a, b) => a - b);
+  b.sort((a, b) => b - a);
 
-  codeArray.forEach((e, i) => {
-    if (mode === 0) {
-      e !== "1" && i % 2 === 0 ? (ret += e) : e === "1" ? (mode = 1) : null;
-    } else {
-      e !== "1" && i % 2 === 1 ? (ret += e) : e === "1" ? (mode = 0) : null;
-    }
-  });
+  for (let i = 0; i < a.length; i++) {
+    answer += a[i] * b[i];
+  }
 
-  return ret === "" ? "EMPTY" : ret;
+  return answer;
 }
 
-console.log("enswer1:", solution("ab1cd1ef1gh"));
+console.log("answer2:", solution([1, 4, 2], [5, 4, 4]));
+console.log("answer3:", solution([1, 2], [3, 4]));
