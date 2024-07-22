2afe9fb1a56ed2a93bd56357277513ce85ca438c
sh5080 <prpn97@gmail.com>
2024-07-22 13:24:36 +0900

    프로그래머스 피보나치 수

--- practice.js
+++ practice.js
@@ -1,22 +1,33 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-// ** 내가 푼 답
-function calculateOneCount(number) {
-  let oneCount = number.toString(2).replaceAll("0", "").length;
-  return oneCount;
-}
+// ** 내가 푼 답 (무한으로 빠지는 오류가 있음.)
+// function solution(n) {
+//   return func(n - 2) + func(n - 1);
+// }
+// function func(n) {
+//   if (n === 0) {
+//     return 0;
+//   } else if (n === 1) {
+//     return 1;
+//   } else {
+//     return solution(n);
+//   }
+// }
+
 function solution(n) {
-  let origin = calculateOneCount(n);
-  let next = "";
-  let i = 1;
-  let temp = 0;
-  while (next != origin) {
-    temp = n + i;
-    next = calculateOneCount(temp);
-    i++;
+  const num = 1234567;
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let a = 0,
+    b = 1;
+  for (let i = 2; i <= n; i++) {
+    let temp = (a + b) % num;
+    a = b;
+    b = temp;
   }
-  return temp;
+  return b;
 }
 
-console.log("answer1:", solution(78));
+console.log("answer1:", solution(5));
