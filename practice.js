ad8801f48f2709771fa1ad9a412b61194c8511ad
sh5080 <prpn97@gmail.com>
2024-07-23 15:31:50 +0900

    프로그래머스 점프와 순간 이동

--- practice.js
+++ practice.js
@@ -1,36 +1,18 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-// ** 내가 푼 답 (효율성 문제 생김)
 function solution(n) {
-  let temp = n;
-  let i = 0;
+  let batteryUsage = 0;
 
-  while (i < temp.length - 1) {
-    if (temp[i] === temp[i + 1]) {
-      temp = temp.slice(0, i) + temp.slice(i + 2);
-      i = Math.max(i - 1, 0);
+  while (n > 0) {
+    if (n % 2 === 0) {
+      n /= 2;
     } else {
-      i++;
+      n -= 1;
+      batteryUsage++;
     }
   }
 
-  if (temp.length > 0) {
-    return 0;
-  } else {
-    return 1;
-  }
+  return batteryUsage;
 }
-// 개선 답안
-function solution(n) {
-  const stack = [];
-  for (let i = 0; i < n.length; i++) {
-    if (stack.length > 0 && stack[stack.length - 1] === n[i]) {
-      stack.pop();
-    } else {
-      stack.push(n[i]);
-    }
-  }
-  return stack.length === 0 ? 1 : 0;
-}
-console.log("answer2:", solution("dcdcaa"));
+console.log("answer1:", solution(6));
