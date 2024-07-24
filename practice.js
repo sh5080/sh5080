41773b181f3667e5b3786c353d5e84a5452e93ef
sh5080 <prpn97@gmail.com>
2024-07-24 23:16:57 +0900

    프로그래머스 구명보트

--- practice.js
+++ practice.js
@@ -1,18 +1,48 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(n) {
-  let batteryUsage = 0;
+// 내가 푼 답 (해결못함.. )
+function solution(people, limit) {
+  let sorted = people.sort((a, b) => a - b);
+  let sliced = sorted;
+  let prev = 0;
+  let boat = 0;
+  let i = 0;
 
-  while (n > 0) {
-    if (n % 2 === 0) {
-      n /= 2;
-    } else {
-      n -= 1;
-      batteryUsage++;
+  while (sliced.length > 0) {
+    if (sliced.length === 1) {
+      boat++;
+      return boat;
     }
+
+    prev += sorted[i];
+    if (prev === limit) {
+      sliced = sorted.slice(i + 1, sorted.length);
+      boat++;
+      prev = 0;
+    } else if (prev > limit) {
+      sliced = sorted.slice(i, sorted.length);
+      boat++;
+    }
+    i++;
+  }
+  return boat;
+}
+// gpt
+function solution(people, limit) {
+  people.sort((a, b) => a - b);
+  let i = 0;
+  let j = people.length - 1;
+  let boats = 0;
+
+  while (i <= j) {
+    if (people[i] + people[j] <= limit) {
+      i++;
+    }
+    j--;
+    boats++;
   }
 
-  return batteryUsage;
+  return boats;
 }
-console.log("answer1:", solution(6));
+console.log("answer1:", solution([70, 50, 80, 50], 100));
