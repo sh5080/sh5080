fb7f17e5dc8897312a3b6abb3bae545b83dfe0c0
sh5080 <prpn97@gmail.com>
2024-08-11 17:10:17 +0900

    프로그래머스: 영어 끝말잇기 수정

--- practice.js
+++ practice.js
@@ -7,35 +7,28 @@ function solution(n, words) {
 
   for (let i = 1; i < words.length; i++) {
     if (words[i][0] !== words[i - 1][words[i - 1].length - 1]) {
-      first = (i % n) + 1;
-      second = Math.floor(i / n) + 1;
+      if (i === 1 && 1 / n <= 1) {
+        first = i + 1;
+        second = Math.floor(i / n) + 1;
+      } else {
+        first = (i % n) + 1;
+        second = Math.floor(i / n) + 1;
+      }
       return [first, second];
     }
-  }
-  let set = new Set();
-  for (let j = 0; j < words.length; j++) {
-    set.add(words[j]);
-    if (set.size === j) {
-      first = (j % n) + 1;
-      second = Math.floor(j / n) + 1;
-      return [first, second];
+    let set = new Set();
+    for (let j = 0; j < words.length; j++) {
+      set.add(words[j]);
+      if (set.size === j) {
+        first = (j % n) + 1;
+        second = Math.floor(j / n) + 1;
+        return [first, second];
+      }
     }
   }
-
   return [first, second];
 }
 console.log(
   "answer1:",
-  solution(3, [
-    "tank",
-    "kick",
-    "know",
-    "wheel",
-    "land",
-    "dream",
-    "mother",
-    "robot",
-    "tank",
-  ])
+  solution(2, ["hello", "one", "even", "never", "now", "world", "draw"])
 );
-// console.log("answer2:", solution([1, 2, 3]));
