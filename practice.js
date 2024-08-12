a02bf3a54edc683f50d92033fbf215cca67ae800
sh5080 <prpn97@gmail.com>
2024-08-12 11:34:29 +0900

    프로그래머스: 예상 대진표

--- practice.js
+++ practice.js
@@ -1,34 +1,27 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(n, words) {
-  let first = 0;
-  let second = 0;
-
-  for (let i = 1; i < words.length; i++) {
-    if (words[i][0] !== words[i - 1][words[i - 1].length - 1]) {
-      if (i === 1 && 1 / n <= 1) {
-        first = i + 1;
-        second = Math.floor(i / n) + 1;
-      } else {
-        first = (i % n) + 1;
-        second = Math.floor(i / n) + 1;
-      }
-      return [first, second];
+function solution(n, a, b) {
+  let count = 1;
+  while (n > 1) {
+    if (Math.abs(a - b) === 1 && Math.min(a, b) % 2 === 1) {
+      return count;
     }
-    let set = new Set();
-    for (let j = 0; j < words.length; j++) {
-      set.add(words[j]);
-      if (set.size === j) {
-        first = (j % n) + 1;
-        second = Math.floor(j / n) + 1;
-        return [first, second];
-      }
+    n = n / 2;
+    count++;
+
+    if (a % 2 === 0) {
+      a = a / 2;
+    } else {
+      a = (a + 1) / 2;
+    }
+
+    if (b % 2 === 0) {
+      b = b / 2;
+    } else {
+      b = (b + 1) / 2;
     }
   }
-  return [first, second];
+  return count;
 }
-console.log(
-  "answer1:",
-  solution(2, ["hello", "one", "even", "never", "now", "world", "draw"])
-);
+console.log("answer1:", solution(16, 4, 9));
