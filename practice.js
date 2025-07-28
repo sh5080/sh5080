bbeb8e7d11bc90453627f2dad6bf797e24d3d8f0
sh5080 <prpn97@gmail.com>
2025-07-28 17:28:44 +0900

    leetcode: twoSum

--- practice.js
+++ practice.js
@@ -1,27 +1,14 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function solution(n, a, b) {
-  let count = 1;
-  while (n > 1) {
-    if (Math.abs(a - b) === 1 && Math.min(a, b) % 2 === 1) {
-      return count;
-    }
-    n = n / 2;
-    count++;
-
-    if (a % 2 === 0) {
-      a = a / 2;
-    } else {
-      a = (a + 1) / 2;
-    }
-
-    if (b % 2 === 0) {
-      b = b / 2;
-    } else {
-      b = (b + 1) / 2;
-    }
+function twoSum(nums, target) {
+  for(let i = 0; i < nums.length; i++){
+      for(let j = i + 1; j < nums.length; j++){
+          if(nums[i] + nums[j] === target){
+              return [i, j];
+          }
+      }
   }
-  return count;
-}
-console.log("answer1:", solution(16, 4, 9));
+  return [];
+};
+console.log(twoSum([2,5,5,11], 10));
