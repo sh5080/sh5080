ff1438abcaf166d32334899e6573750d3b55a3b9
sh5080 <prpn97@gmail.com>
2025-07-29 14:49:19 +0900

    leetcode: roman-to-integer

--- practice.js
+++ practice.js
@@ -2,13 +2,28 @@ const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
 /**
- * @param {number} x
- * @return {boolean}
+ * @param {string} s
+ * @return {number}
  */
-var isPalindrome = function (x) {
-  const str = x.toString().split("").join("");
-  const reversed = x.toString().split("").reverse().join("");
-
-  return str === reversed;
+var romanToInt = function (s) {
+  const int = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let arr = [];
+  for (let i = 0; i < s.length; i++) {
+    arr.push(int[s[i]]);
+  }
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= arr[i + 1] || i === arr.length - 1) result = result + arr[i];
+    if (arr[i] < arr[i + 1]) result = result - arr[i];
+  }
+  return result;
 };
-console.log(isPalindrome(121));
+console.log(romanToInt("MCMXCIV"));
