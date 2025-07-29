74b7f954dae352726dbac16ef200dbac3713db9f
sh5080 <prpn97@gmail.com>
2025-07-29 13:39:54 +0900

    leetcode: addTwoNumbers

--- practice.js
+++ practice.js
@@ -1,14 +1,29 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function twoSum(nums, target) {
-  for(let i = 0; i < nums.length; i++){
-      for(let j = i + 1; j < nums.length; j++){
-          if(nums[i] + nums[j] === target){
-              return [i, j];
-          }
-      }
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+function reverse(list) {
+  let result = [];
+  const val = String(list);
+  console.log(val);
+
+  for (let i = list.length - 1; i > 0; i--) {
+    console.log(i);
+    result.push(list[i]);
   }
-  return [];
+  console.log(result);
+  return result;
+}
+var addTwoNumbers = function (l1, l2) {
+  const list1 = reverse(l1).join("");
+  const list2 = reverse(l2).join("");
+  const temp = Number(list1) + Number(list2);
+
+  console.log(temp);
+  return temp;
 };
-console.log(twoSum([2,5,5,11], 10));
+addTwoNumbers([2, 4, 3], [5, 6, 4]);
