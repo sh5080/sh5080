3fa93022fbc0fd659293557872d7195b432761e7
sh5080 <prpn97@gmail.com>
2025-07-30 11:01:23 +0900

    leetcode: removeDuplicates

--- practice.js
+++ practice.js
@@ -1,35 +1,19 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function ListNode(val, next) {
-  this.val = val === undefined ? 0 : val;
-  this.next = next === undefined ? null : next;
-}
 /**
- * @param {ListNode} list1
- * @param {ListNode} list2
- * @return {ListNode}
+ * @param {number[]} nums
+ * @return {number}
  */
-var mergeTwoLists = function (list1, list2) {
-  let current1 = list1;
-  let current2 = list2;
-  let temp = [];
-  while (current1 != null) {
-    temp.push(current1.val);
-    current1 = current1.next;
-  }
-  while (current2 != null) {
-    temp.push(current2.val);
-    current2 = current2.next;
-  }
-  temp.sort((a, b) => a - b);
-  let head = new ListNode();
-  let result = head;
-  temp.map((val) => {
-    result.next = new ListNode(Number(val));
-    result = result.next;
-  });
+var removeDuplicates = function (nums) {
+  let i = 0;
 
-  return head.next;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
 };
-console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]));
+console.log(removeDuplicates([1, 1, 2]));
