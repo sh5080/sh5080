7c92a0528c6dd20b27b227c9aec069729fe3c43d
sh5080 <prpn97@gmail.com>
2025-07-30 11:24:58 +0900

    leetcode: removeElement

--- practice.js
+++ practice.js
@@ -3,17 +3,18 @@ const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
 /**
  * @param {number[]} nums
+ * @param {number} val
  * @return {number}
  */
-var removeDuplicates = function (nums) {
-  let i = 0;
-
-  for (let j = 1; j < nums.length; j++) {
-    if (nums[j] !== nums[i]) {
-      i++;
-      nums[i] = nums[j];
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      while ((temp = nums[i]) === val) {
+        nums.splice(i, 1);
+      }
     }
   }
-  return i + 1;
+
+  return nums.length;
 };
-console.log(removeDuplicates([1, 1, 2]));
+console.log(removeElement([3, 2, 2, 3], 3));
