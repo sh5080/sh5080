974103e57be3582f6dc58d671a0cca9a4f793e7c
sh5080 <prpn97@gmail.com>
2025-08-04 13:43:36 +0900

    neetcode: productExceptSelf

--- practice.js
+++ practice.js
@@ -2,31 +2,23 @@ const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
 
-/**
-* @param {string[]} strs
-* @returns {string}
-*/
-function encode(strs) {
-   if(!strs.length) return "[]"
-   let resultStr = "["
-   strs.map((str)=>{
-       resultStr = resultStr + `"${str}",` 
-   })
-   resultStr = resultStr.slice(0,-1) + "]"
-   return resultStr
+
+function productExceptSelf(nums) {
+  const result = []
+  let temp = 1
+  nums.map((num, index) => {
+  const withoutCurrent = nums.filter((_, i) => i !== index)
+  
+  for(let i = 0; i < withoutCurrent.length; i++){
+      temp = temp*withoutCurrent[i]
+      if(i === withoutCurrent.length-1){
+          result.push(temp)
+          temp = 1
+          }
+  }
+  return temp;
+  })
+ return result
 }
 
-/**
-* @param {string} str
-* @returns {string[]}
-*/
-function decode(str) {
-   if(str === "[]") return []
-   let newArr = str.slice(1,str.length-1).split(`","`)
-   newArr[0] = newArr[0].slice(1,newArr[0].length)
-   newArr[newArr.length-1] = newArr[newArr.length-1].slice(0,-1)
-    return newArr
-}
-
-console.log(encode(["Hello", "World"]));
-console.log(decode(encode(["Hello", "World"])));
+console.log(productExceptSelf([1,2,3,4]));
