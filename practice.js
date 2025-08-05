59fc228421d7fd6b3e95b558bff5a2cafa0ce628
sh5080 <prpn97@gmail.com>
2025-08-05 17:14:44 +0900

    neetcode: max-water-container

--- practice.js
+++ practice.js
@@ -2,31 +2,31 @@ const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
 
-function threeSum(nums) {
-    const temp = []
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i+1; j < nums.length; j++){
-            for(let k = j+1; k < nums.length; k++){
-                if(nums[i] + nums[j] + nums[k] === 0){
-                    let sorted = [nums[i], nums[j], nums[k]].sort((a,b)=> a-b)
-                    temp.push(sorted)
-                }
-            }
+function maxArea(heights) {
+    const sorted = [...heights].sort((a,b)=> b-a)
+
+    function getAmount(startIdx, endIdx){
+        const start = heights[startIdx]
+        const end = heights[endIdx]
+        let length = endIdx - startIdx > 0 ? endIdx - startIdx : 0 - (endIdx - startIdx)
+
+        const amount = start > end ? end*length : start*length
+        return amount
+    }
+    let tempAmount = 0
+
+    for(let i = 0; i < heights.length; i++){
+        for(let j = 1; j < heights.length; j++){
+
+        let startIdx = heights.indexOf(sorted[i])
+        let endIdx = heights.lastIndexOf(sorted[j])
+
+        let amount = getAmount(startIdx, endIdx)
+        if(tempAmount < amount) tempAmount = amount
+
         }
     }
-
-    const numSet = new Set()
-
-    temp.map((str)=>{
-    let sortedStr = JSON.stringify(str)
-      if(!numSet.has(sortedStr)){
-        numSet.add(sortedStr);
-    }
-    })
-const stringArray = Array.from(numSet);
-const numberArrays = stringArray.map(str => JSON.parse(str));
-
-return numberArrays
+    return tempAmount
 }
 
-console.log(threeSum([-1,0,1,2,-1,-4]))
+console.log(maxArea([1,8,6,2,5,4,8,3,7]))
