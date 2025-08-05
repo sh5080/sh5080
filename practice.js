18c0e7e8018f33adcb868695f96d1fa8e4f5c34d
sh5080 <prpn97@gmail.com>
2025-08-05 14:46:37 +0900

    neetcode: longest-consecutive-sequence

--- practice.js
+++ practice.js
@@ -1,24 +1,30 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
+function longestConsecutive(nums) {
+    if(!nums.length) return 0
+    const sorted = nums.sort((a,b)=> a-b)
+    
+    let firstNum = sorted[0]
+    let tempNum = firstNum
+    let count = 1
+    let arr = []
+    for(let i = 1; i < sorted.length; i++){
+        if(tempNum === sorted[i]){
+            tempNum = sorted[i]
+            continue
+        }else if(tempNum + 1 === sorted[i]){
+            tempNum = sorted[i]
+            count++
+        }else {
+            tempNum = sorted[i]
+            arr.push(count)
+            count = 1
+        }
 
-
-function productExceptSelf(nums) {
-  const result = []
-  let temp = 1
-  nums.map((num, index) => {
-  const withoutCurrent = nums.filter((_, i) => i !== index)
-  
-  for(let i = 0; i < withoutCurrent.length; i++){
-      temp = temp*withoutCurrent[i]
-      if(i === withoutCurrent.length-1){
-          result.push(temp)
-          temp = 1
-          }
-  }
-  return temp;
-  })
- return result
+    }
+    arr.push(count)
+    return arr.sort((a,b)=> a-b)[arr.length-1]
 }
 
-console.log(productExceptSelf([1,2,3,4]));
+console.log(longestConsecutive([100,4,200,1,3,2]))
