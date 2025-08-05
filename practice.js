6c3455a58a40d69e17090624b0dcef8f1d328212
sh5080 <prpn97@gmail.com>
2025-08-05 16:15:51 +0900

    neetcode: three-integer-sum

--- practice.js
+++ practice.js
@@ -1,12 +1,32 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString().trim().split("\n");
 
-function twoSum(numbers, target) {
-    for(let i = 0; i < numbers.length; i++){
-        for(let j = i; j < numbers.length; j++){
-            if(numbers[i] + numbers[j] === target) return [i+1,j+1]
+
+function threeSum(nums) {
+    const temp = []
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
+            for(let k = j+1; k < nums.length; k++){
+                if(nums[i] + nums[j] + nums[k] === 0){
+                    let sorted = [nums[i], nums[j], nums[k]].sort((a,b)=> a-b)
+                    temp.push(sorted)
+                }
+            }
         }
     }
+
+    const numSet = new Set()
+
+    temp.map((str)=>{
+    let sortedStr = JSON.stringify(str)
+      if(!numSet.has(sortedStr)){
+        numSet.add(sortedStr);
+    }
+    })
+const stringArray = Array.from(numSet);
+const numberArrays = stringArray.map(str => JSON.parse(str));
+
+return numberArrays
 }
 
-console.log(twoSum([2,7,11,15],9))
+console.log(threeSum([-1,0,1,2,-1,-4]))
